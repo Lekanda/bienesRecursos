5b1a0cc74fd9b64b01d5d2301b89ca9e17f849c0
lekanda <abernaolao@gmail.com>
2021-03-27 23:47:01 +0100

    302.Validar el Formulario.

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -2,26 +2,16 @@
     // Base de Datos
     require '../../includes/config/database.php';
     $db = conectarDB();
-    // var_dump($db);
-    
-    // echo "<pre>";
-    // var_dump($_POST); // Los valores que se han mandado por POST
-    // var_dump($_GET); // LOs valores que se han mandado por GET. Se ven en URL.
-    // echo "</pre>";
 
-    // Informacion del Servidor,
-    // echo "<pre>";
-    // var_dump($_SERVER["SERVER_SOFTWARE"]);
-    // var_dump($_SERVER["REQUEST_METHOD"]);
-    // echo "</pre>";
+    // Arreglo con mensajes de errores
+    $errores = [];
+
+    // Ejecuta el codigo despues de que el usuario envie el formulario
+    
+
 
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // echo "<pre>";
-        // var_dump($_POST); // Los valores que se han mandado por POST
-        // var_dump($_POST["titulo"]); // Los valores que se han mandado por POST
-        // echo "</pre>";
-
 
         $titulo = $_POST["titulo"];
         $precio = $_POST["precio"];
@@ -32,22 +22,50 @@
         $vendedorId = $_POST["vendedor"];
 
 
-        // Insertar en la DB
-        $query = "INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento,vendedorId) VALUES ( '$titulo', '$precio','$descripcion','$habitaciones','$wc','$estacionamiento','$vendedorId')";
-        // echo $query;
-        $resultado = mysqli_query($db,$query);
-        if($resultado){
-            echo "Insertado correctamente";
+        // Validar que no vaya vacio
+        if (!$titulo) {
+            // $errores[] => añade al arreglo $errores
+            $errores[] = "Debes añadir un titulo";
         }
+        if (!$precio) {
+            $errores[] = "Debes añadir un precio";
+        }
+        if (strlen($descripcion) < 20) {
+            $errores[] = "Debes añadir una descripcion";
+        }
+        if (!$habitaciones) {
+            $errores[] = "Debes añadir un numero de Habitaciones";
+        }
+        if (!$wc) {
+            $errores[] = "Debes añadir un numero de Baños";
+        }
+        if (!$estacionamiento) {
+            // $errores[] => añade al arreglo $errores
+            $errores[] = "Debes añadir un numero de plazas de aparcamiento";
+        }
+        if (!$vendedorId) {
+            // $errores[] => añade al arreglo $errores
+            $errores[] = "Debes añadir un Identificador de vendedor";
+        }
+        // echo "<pre>";
+        // var_dump($errores);
+        // echo "</pre>";
+        // exit;
 
+
+        // Comprobar que no haya errores en arreglo $errores. Comprueba que este VACIO (empty).
+        if (empty($errores)) {
+            // Insertar en la DB
+            $query = "INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento,vendedorId) VALUES ( '$titulo', '$precio','$descripcion','$habitaciones','$wc','$estacionamiento','$vendedorId')";
+
+            $resultado = mysqli_query($db,$query);
+            
+            if($resultado){
+                echo "Insertado correctamente";
+            }
+        }
+        
     }
-
-
-    
-
-    
-    
-
 
     require '../../includes/funciones.php';
     incluirTemplate('header');
@@ -61,6 +79,12 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <?php foreach ($errores as $error) : ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?> 
 
         <form method="POST" class="formulario" action="/admin/propiedades/crear.php">
             <fieldset>
@@ -84,13 +108,13 @@
                 <legend>Informacion Propiedad</legend>
 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" name="habitaciones"  placeholder="Numero Habitaciones Propiedad" min="1" max="9" step="2">
+                <input type="number" id="habitaciones" name="habitaciones"  placeholder="Numero Habitaciones 1-9" min="1" max="9" step="1">
 
                 <label for="wc">WC:</label>
-                <input type="number" id="wc" name="wc" placeholder="Numero wc Propiedad" min="1" max="9" step="1">
+                <input type="number" id="wc" name="wc" placeholder="Numero wc Propiedad 1-9" min="1" max="9" step="1">
 
                 <label for="estacionamiento">Estacionamiento:</label>
-                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Estacionamientos para Coches" min="1" max="9" step="2">
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Estacionamientos para Coches 1-9" min="1" max="9" step="1">
 
             </fieldset>
 
@@ -98,6 +122,7 @@
                 <legend>Vendedor</legend>
 
                 <select name="vendedor">
+                    <option value="">-- Seleccione --</option>
                     <option value="1">Juan</option>
                     <option value="2">Pedro</option>
                 </select>
