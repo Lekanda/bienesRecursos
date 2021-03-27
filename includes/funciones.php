336a4bfd716bb9195441d6d8b4da0adc827b3475
lekanda <abernaolao@gmail.com>
2021-03-27 14:30:38 +0100

    297.Crear archivos para el CRUD.

--- includes/funciones.php
+++ includes/funciones.php
@@ -3,5 +3,6 @@
 require 'app.php';
 
 function incluirTemplate( string $nombre, bool $inicio = false){
+    // echo TEMPLATES_URL . "/${nombre}.php";
     include TEMPLATES_URL . "/${nombre}.php";
 }
