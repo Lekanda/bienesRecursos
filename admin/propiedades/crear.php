aac159056eccb5f49699ae2d7b459f8be23f4a30
lekanda <abernaolao@gmail.com>
2021-03-28 21:21:16 +0200

    309.Generar nombres unicos para imagen en servidor.

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -86,9 +86,9 @@
         // var_dump($errores);
         // echo "</pre>";
 
-        echo "<pre>";
-        var_dump($_FILES);
-        echo "</pre>";
+        // echo "<pre>";
+        // var_dump($_FILES);
+        // echo "</pre>";
 
 
         // Comprobar que no haya errores en arreglo $errores. Comprueba que este VACIO (empty).
@@ -96,22 +96,22 @@
 
             /**Subida de Archivos**/
             // Crear una carpeta
-            $carpetaImagenes = '../../imagenes';
+            $carpetaImagenes = '../../imagenes/';
 
             if (!is_dir($carpetaImagenes)) {
                 mkdir($carpetaImagenes);
             }
 
+            // Generar un nombre unico
+            $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+            var_dump($nombreImagen);
+
             // Subir la imagen
-            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . "/archivo.jpg" );
-            exit;
-
-
-
-
+            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes  . $nombreImagen);
 
             // Insertar en la DB
-            $query = "INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento,creado,vendedorId) VALUES ( '$titulo', '$precio','$descripcion','$habitaciones','$wc','$estacionamiento','$creado','$vendedorId')";
+            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ( '$titulo', '$precio','$nombreImagen','$descripcion','$habitaciones','$wc','$estacionamiento','$creado','$vendedorId')";
+            echo $query;
 
             $resultado = mysqli_query($db,$query);
             
