1e44aff9499be0e7d9992097e21ead14db02f3c0
lekanda <abernaolao@gmail.com>
2021-03-29 19:57:06 +0200

    313.Actualizar Propiedad Pt-1.

--- admin/index.php
+++ admin/index.php
@@ -1,48 +1,67 @@
-<?php 
-    // echo "<pre>";
-    // var_dump($_GET);
-    // echo "</pre>";
-    // exit;
+<?php
 
-    $resultado = $_GET['resultado'] ?? null;
+// Importar la conexion de la DB
+require '../includes/config/database.php';
+$db = conectarDB();
 
-    require '../includes/funciones.php';
-    incluirTemplate('header');
-?>  
-    <main class="contenedor seccion">
-        <h1>Administrador de Bienes Raices</h1>
-        <?php if (intval($resultado) === 1) : ?>
-            <p class="alerta exito">Anuncio creado correctamente</p>
-        <?php endif; ?>
+//Escribir el Query
+$query = "SELECT * FROM propiedades";
 
-        <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+// Consultar la DB
+$resultadoConsulta = mysqli_query($db, $query);
 
-        <table class="propiedades">
-            <thead>
+
+// Muestra mensaje condicional
+$resultado = $_GET['resultado'] ?? null;
+
+// Incluye un template
+require '../includes/funciones.php';
+incluirTemplate('header');
+?>
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raices</h1>
+    <?php if (intval($resultado) === 1) : ?>
+        <p class="alerta exito">Anuncio creado correctamente</p>
+    <?php endif; ?>
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody class="cuerpo-tabla-propiedades">
+            <!-- Mostrar los resultados -->
+            <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)) : ?>
                 <tr>
-                    <th>ID</th>
-                    <th>Titulo</th>
-                    <th>Imagen</th>
-                    <th>Precio</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Casa en la Playa</td>
-                    <td><img src="/imagenes/3c26578b617f0dfa79055240f90fd726.jpg" class="imagen-tabla"></td>
-                    <td>1200000€</td>
+                    <td> <?php echo $propiedad['id']; ?> </td>
+                    <td><?php echo $propiedad['titulo']; ?></td>
+
+                    <td class="imagen-tabla"><img class="imagen-tabla" src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla"></td>
+
+                    <td><?php echo $propiedad['precio']; ?>€</td>
                     <td>
-                        <a href="#" class="boton-rojo-block" >Eliminar</a>
-                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
-            </tbody>
-        </table>
+            <?php endwhile ?>
+        </tbody>
+    </table>
 
-    </main>
+</main>
 
-<?php 
-    incluirTemplate('footer');
+<?php
+
+// Cerrar la conexion
+mysqli_close($db);
+
+
+incluirTemplate('footer');
 ?>
