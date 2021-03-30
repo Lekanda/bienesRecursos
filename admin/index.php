9f2954c9c5334610edfcf6158015393908de1b50
lekanda <abernaolao@gmail.com>
2021-03-30 19:52:10 +0200

    317.Eliminar registro de DB.

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 // Importar la conexion de la DB
 require '../includes/config/database.php';
 $db = conectarDB();
@@ -14,6 +15,31 @@ $resultadoConsulta = mysqli_query($db, $query);
 // Muestra mensaje condicional, si no hay lo pone como null
 $resultado = $_GET['resultado'] ?? null;
 
+
+//
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+    
+    if ($id) {
+        // Elimina el archivo
+        $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
+
+        unlink('../imagenes/' . $propiedad['imagen']);
+
+        // Elimina la propiedad
+        $query = "DELETE FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db,$query);
+        if ($resultado) {
+            header('location: /admin?resultado=3' );
+        }
+    }
+}
+
+
+
 // Incluye un template
 require '../includes/funciones.php';
 incluirTemplate('header');
@@ -24,6 +50,8 @@ incluirTemplate('header');
             <p class="alerta exito">Anuncio Creado Correctamente</p>
         <?php elseif (intval($resultado) === 2) :?>
             <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+        <?php elseif (intval($resultado) === 3) :?>
+            <p class="alerta exito">Anuncio Borrado Correctamente</p>
     <?php endif; ?>
 
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
@@ -38,8 +66,7 @@ incluirTemplate('header');
                 <th>Acciones</th>
             </tr>
         </thead>
-        <tbody class="cuerpo-tabla-propiedades">
-            <!-- Mostrar los resultados -->
+        <tbody class="cuerpo-tabla-propiedades"><!-- Mostrar los resultados -->
             <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)) : ?>
                 <tr>
                     <td> <?php echo $propiedad['id']; ?> </td>
@@ -49,7 +76,11 @@ incluirTemplate('header');
 
                     <td><?php echo $propiedad['precio']; ?>€</td>
                     <td>
-                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
                         <a href="propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
                     </td>
                 </tr>
