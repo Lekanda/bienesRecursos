581714703b92bc2aed996b2b119692114365eb01
lekanda <abernaolao@gmail.com>
2021-04-02 14:24:01 +0200

    327.Hashear el Password.

--- usuario.php
+++ usuario.php
@@ -8,11 +8,12 @@ $db = conectarDB();
 $email = "a@a.com";
 $password = "123456";
 
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
+var_dump($passwordHash);
 
 // Querypara crear el usuario
-$query = "INSERT INTO usuarios (email,password) VALUES ('${email}','${password}')";
-// echo $query;
+$query = "INSERT INTO usuarios (email,password) VALUES ('${email}','${passwordHash}')";
 
 // agregar a la DB
 mysqli_query($db, $query);
